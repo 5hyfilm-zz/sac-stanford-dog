25a21503ba69dba816cc62106687962be9b641bd
Waranthorn Chansawang <filmer2002@hotmail.com>
2020-07-03 18:01:39 +0700

    build web

--- web/app.py
+++ web/app.py
@@ -12,10 +12,10 @@ import uuid
 import base64
 
 IMG_WIDTH, IMG_HEIGHT = 224, 224
-model_path = ''
+model_path = 'web/model/standford_sac_dog_model.h5'
 model = load_model(model_path)
 
-UPLOAD_FOLDER = './upload_folder'
+UPLOAD_FOLDER = 'web/upload_folder'
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'JPG', 'JPEG', 'png'])
 
 app = Flask(__name__)
@@ -24,21 +24,36 @@ def get_as_base64(url):
     return base64.b64encode(requests.get(url).content)
 
 def predict(file):
-    with graph.as_default():
-        x = load_img(file, target_size=(IMG_WIDTH, IMG_HEIGHT))
-        x = img_to_array(x)
-        x = np.true_divide(x, 255)
-        x = np.expand_dims(x, axis=0)
-        array = model.predict(x)
-        result = array[0]
-        answer = np.argmax(result)
-        if answer == 0:
-            print("Label: Shih-Tzu")
-        elif answer == 1:
-            print("Label: papillon")
-        elif answer == 2:
-            print('Label: beagle')
-        return answer
+    x = load_img(file, target_size=(IMG_WIDTH, IMG_HEIGHT))
+    x = img_to_array(x)
+    x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+    array = model.predict(x)
+    result = array[0]
+    answer = np.argmax(result)
+
+    if answer == 0:
+        print('Label: Beagle')
+    elif answer == 1:   
+        print('Label: Chihuahua')
+    elif answer == 2:
+        print('Label: French Bulldog')
+    elif answer == 3:
+        print('Label: Golden Retriever')
+    elif answer == 4:
+        print('Label: Pomeranian')
+    elif answer == 5:
+        print('Label: Pug')
+    elif answer == 6:
+        print('Label: Rottweiler')
+    elif answer == 7:
+        print('Label: Shih-Tzu')
+    elif answer == 8:
+        print('Label: Siberian Husky')
+    elif answer == 9:
+        print('Label: Yorkshire Terrier')
+        
+    return answer
 
 def my_random_string(string_length=10):
     """Returns a random string of length string_length."""
@@ -56,7 +71,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def template_test():
-    return render_template('index.html', label_th='', label_eng='')
+    return render_template('index.html', label='')
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -72,26 +87,33 @@ def upload_file():
             file.save(file_path)
             result = predict(file_path)
             if result == 0:
-                label = 'beagle'
+                label = 'Beagle'
             elif result == 1:
-                label =	'chihuahua'
-            elif result = 2:
-                label = 'french_bulldog':
-            elif result = 3:
-                label = 'golden_retriever':
-            elif result = 4:
-                label = 'pomeranian'
-            elif result = 5:
-                label = 'pug'
+                label =	'Chihuahua'
+            elif result == 2:
+                label = 'French Bulldog'
+            elif result == 3:
+                label = 'Golden Retriever'
+            elif result == 4:
+                label = 'Pomeranian'
+            elif result == 5:
+                label = 'Pug'
+            elif result == 6:
+                label = 'Rottweiler'
+            elif result == 7:
+                label = 'Shih-Tzu'
+            elif result == 8:
+                label = 'Siberian Husky'
+            elif result == 9:
+                label = 'Yorkshire Terrier'
             
-
             print(result)
             print(file_path)
             filename = my_random_string(6) + filename
 
             os.rename(file_path, os.path.join(app.config['UPLOAD_FOLDER'], filename))
             print("--- %s seconds ---" % str (time.time() - start_time))
-            return render_template('arm.html', label_th=label_th, label_eng=label_eng, imagesource='../upload_folder/' + filename)
+            return render_template('index.html', label=label, imagesource='upload_folder/' + filename)
 
 from flask import send_from_directory
 
